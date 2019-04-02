8331c638ae9ad7785938b612d508bbf806d52b36
Administrator <administrator@CN-4.lan>
2019-04-02 17:56:56 +0100

    copied app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TimeTable from './components/user/Rota_Timesheet/TimeTable';
 import './App.css';
 
 class App extends Component {
+  state={currentUser:2}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+              <TimeTable userid={this.state.currentUser}/>
+
       </div>
     );
   }
