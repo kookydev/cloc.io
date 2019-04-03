2e680bcb9a429f86a6155f948c8a86f260cdda9d
DuncanRitchie <duncanritchie@btinternet.com>
2019-04-03 12:04:45 +0100

    Got the rota/times working, but we still have extraneous files.

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import TimeTable from './components/user/Rota_Timesheet/TimeTable';
 import './App.css';
 
 class App extends Component {
-  state={currentUser:2}
+  state={
+    currentUser: 2,
+    rotaOrTimes: "rota"}
   render() {
     return (
       <div className="App">
-              <TimeTable userid={this.state.currentUser}/>
+              <TimeTable userid={this.state.currentUser} rotaOrTimes={this.state.rotaOrTimes}/>
 
       </div>
     );
