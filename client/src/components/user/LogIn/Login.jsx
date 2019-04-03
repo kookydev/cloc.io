cef4fcb217879c909d88e038f6834c9d68913d93
Code Nation 13 <codenation@CN-13.local>
2019-04-03 10:24:22 +0100

    login page

--- client/src/components/user/LogIn/Login.jsx
+++ client/src/components/user/LogIn/Login.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const Login = (props) => {
-    return (  );
-}
- 
+const Login = props => {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("Login was clicked");
+  }
+
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Login</h2>
+          <input type="text" placeholder="Enter Username" required />
+
+          <h2>Password</h2>
+          <input type="password" placeholder="Enter Password" required />
+          <br />
+          <button type="submit" onClick={handleClick} href=" ">
+            Login
+          </button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember" />
+            Remember Me
+          </label>
+        </div>
+      </div>
+
+      <div>
+        <span>
+          Forgot <a href=" ">password?</a>
+        </span>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
