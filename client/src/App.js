025bfb4fa555aa79b40e97705f97f1fd86e04fa1
KieranLProctor <kieran.lee.proctor@gmail.com>
2019-04-03 14:10:00 +0100

    Regitration is completed via web form

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,50 @@
-import React, { Component } from 'react';
-import TimeTable from './components/user/Rota_Timesheet/TimeTable';
-import './App.css';
+import React, { Component } from "react";
+// import TimeTable from "./components/user/Rota_Timesheet/TimeTable";
+// import Login from "./components/user/LogIn/Login";
+import AddUser from './components/user/AddUser/AddUser';
+import "./App.css";
 
 class App extends Component {
-  state={currentUser:2}
+  state = {
+      forename: null,
+      surname: null,
+      username: null, 
+      password: null,
+      auth_lvl: 2,
+      job_role: null
+    };
+
+  createUser = () => {
+    return fetch(`http://localhost:5000/user`, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        forename: this.state.forename,
+        surname: this.state.surname,
+        username: this.state.username,
+        password: this.state.password,
+        auth_lvl: this.state.auth_lvl,
+        job_role: this.state.job_role
+      })
+    });
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.prevventDefault();
+  }
+
   render() {
     return (
       <div className="App">
-              <TimeTable userid={this.state.currentUser}/>
-
+        {/* <TimeTable userid={this.state.currentUser} /> */}
+        {/* <Login handleChange={this.handleChange} handleSubmit={this.handleSubmit}/> */}
+        <AddUser handleChange={this.handleChange} handleSubmit={this.handleSubmit} createUser={this.createUser} value={this.state.value}/>
       </div>
     );
   }
--- client/src/components/user/LogIn/Login.jsx
+++ client/src/components/user/LogIn/Login.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React from "react";
 
-const Login = (props) => {
-    return (  );
-}
- 
+const Login = props => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Login</h2>
+          <input
+            id="username"
+            type="text"
+            placeholder="Enter Username"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+
+          <h2>Password</h2>
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter Password"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button 
+          type="submit" 
+          onClick={props.handleSubmit}>
+            Login
+          </button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember" />
+            Remember Me
+          </label>
+        </div>
+      </div>
+
+      <div>
+        <span>
+          Forgot <a href=" ">password?</a>
+        </span>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
