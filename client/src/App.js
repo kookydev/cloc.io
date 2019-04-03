7dd4a2c66ae9423136bc73a100bd492f3e468ed9
Code Nation 13 <codenation@CN-13.local>
2019-04-03 15:43:03 +0100

    staff overview done for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
-import TimeTable from "./components/user/Rota_Timesheet/TimeTable";
-import UserData from "./data/posts";
+import RotaTimesheet from "./components/user/RotaTimesheet/RotaTimesheet";
 import "./App.css";
+import UserData from "./data/posts";
+import StaffOverview from "./components/manager/StaffOverview/StaffOverview";
 
 class App extends Component {
-  state = { currentUser: 2 };
+  state = {
+    currentUser: 2,
+    rotaOrTimes: "rota"
+  };
+
   render() {
     return (
       <div className="App">
         <StaffOverview staffData={UserData} />
-=======
-import React, { Component } from 'react';
-import RotaTimesheet from './components/user/RotaTimesheet/RotaTimesheet';
-import './App.css';
-
-class App extends Component {
-  state={
-    currentUser: 2,
-    rotaOrTimes: "rota"
-  }
-  
-  render() {
-    return (
-      <div className="App">
-              <RotaTimesheet userid={this.state.currentUser} rotaOrTimes={this.state.rotaOrTimes}/>
-
->>>>>>> ab463d8f07123c1ac8ab4a78247f34572b56f31a
       </div>
     );
   }
--- client/src/components/manager/StaffOverview/StaffOverview.jsx
+++ client/src/components/manager/StaffOverview/StaffOverview.jsx
@@ -1,21 +1,53 @@
 import React from "react";
+import "./StaffOverview.css";
 
 const StaffOverview = props => {
   return (
     <div>
       <h2>Staff</h2>
-      <button type="button" data-toggle="modal">
-        Open Staff
+      <div class="staff-table">
+        <b>Staff ID</b>
+        <b>Forename</b>
+        <b>Surname</b>
+        <b>Requests</b>
+        <span />
+        {props.staffData.map(item => {
+          return (
+            <>
+              <span>{item.id}</span>
+              <span>{item.forename}</span>
+              <span>{item.surname}</span>
+              <span>{item.requests.length}</span>
+              <span>
+                {item.requests.length > 0 ? (
+                  <button>Authorise</button>
+                ) : (
+                  <span />
+                )}
+              </span>
+            </>
+          );
+        })}
+      </div>
+      <button type="button" data-toggle="modal" data-target="#staffModal">
+        Open Staff Details
       </button>
-      <div class="modal fade">
+
+      {/* Code below for Staff Details Modal*/}
+      {/* <div class="modal fade" id="staffModal" role="dialog">
         <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header">
-              <button type="button" data-dismiss="modal" />
+              <button type="button" data-dismiss="modal"></button>
+              <h4>Staff Member</h4>
             </div>
+            <div>
+            <p>Staff Member Details</p>
+            </div>
+            <button type="button" data-dismiss="modal">Close</button>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
