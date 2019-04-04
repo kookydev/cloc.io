b542409c6d762398c5353a8ad29e73f43714dfff
Connor Rooney <connorrooney.ap@gmail.com>
2019-04-04 11:01:16 +0100

    Moved clocking fucntion into parent component, save app state from mess, will move back later if req.

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,18 @@
 import React, { Component } from 'react';
-import Time from './components/user/ClockInOut/Time';
-import AddUser from './components/user/ClockInOut/AddUser';
 import './App.css';
-import RequestLeave from './components/user/RequestLeave/RequestLeave';
+
+import ClockInOutCont from "./components/user/ClockInOut/ClockInOutCont"
 
 
-//Clock in and out pulling from Time.jsx
 class App extends Component {
   state = {
-    onDuty: false,
-    timeIn: "",
-    timeOut: ""
-  }
 
-   clockingHandler = async(time, onDuty) => {
-    console.log(`the time is ${time}`)
-    this.setState({onDuty: onDuty})
-    console.log(`the onDuty is ${onDuty}`)
-    if (onDuty === true) {
-      await this.setState({timeIn: time})
-    }
-    else {
-      await this.setState({timeOut: time})
-    }
-  };
-  
-  
+  }
 
   render() {
     return (
       <div className="App">
-          <Time onDuty={this.state.onDuty} clockFunc={this.clockingHandler} ciTime={this.state.timeIn} coTime={this.state.timeOut}/>
-          <AddUser/>
+        <ClockInOutCont />
       </div>
     );
   }
