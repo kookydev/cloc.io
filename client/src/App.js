fd6a6444468a8ed697b5a82fac14f0ef4f6c6541
Code Nation 13 <codenation@CN-13.local>
2019-04-05 11:31:48 +0100

    upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,22 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
-import RotaTimesheet from "./components/user/RotaTimesheet/RotaTimesheet";
 import "./App.css";
+
 import HomeScreen from "./components/user/HomeScreen/HomeScreen";
-import StaffDetails from "./components/manager/StaffDetails/StaffDetails";
-import UserData from "./data/posts.json";
-import StaffOverview from "./components/manager/StaffOverview/StaffOverview";
+import NotificationCont from "./components/user/Notifications/NotificationCont";
+import Login from "./components/user/LogIn/Login";
 
 class App extends Component {
   state = {
-    currentUser: 2,
+    currentUser: 3,
     rotaOrTimes: "rota"
   };
 
   render() {
     return (
       <div className="App">
-        <StaffDetails staffData={UserData} />
-=======
-import React, { Component } from 'react';
-import './App.css';
-
-import HomeScreen from './components/user/HomeScreen/HomeScreen'
-import NotificationCont from './components/user/Notifications/NotificationCont'
-
-class App extends Component {
-  state={
-    currentUser: 3,
-    rotaOrTimes: "rota",
-  }
-  
-  
-  render() {
-    return (
-      <div className="App">
-
-      <HomeScreen authLevel={2} />
-      <NotificationCont/> 
-
->>>>>>> 536955736a90ffa058853b1cde40cb34cb62a099
+        <Login />
+        {/* <HomeScreen authLevel={2} />
+      <NotificationCont/>  */}
       </div>
     );
   }
