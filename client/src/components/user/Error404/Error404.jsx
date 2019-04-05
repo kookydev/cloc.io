d7045a78c244aaa5452ac60955375b25fbe85b36
Code Nation 13 <codenation@CN-13.local>
2019-04-05 16:03:26 +0100

    updates to styling of login page

---
+++ client/src/components/user/Error404/Error404.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Error404 = props => {
+  return (
+    <div>
+      <h1>Error 404</h1>
+      <h3>Sorry something went wrong! :(</h3>
+      <p>
+        Refresh the page.
+        <br />
+        If the problem persists please contact an Administrator for support
+      </p>
+    </div>
+  );
+};
