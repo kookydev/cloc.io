cc985543374e7a0d1cb513eb48cf8649a6265a64
Administrator <administrator@CN-7.lan>
2019-04-05 11:05:28 +0100

    commit

--- client/src/components/user/Notifications/Notifications.jsx
+++ client/src/components/user/Notifications/Notifications.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
 const Notifications  = (props) => {
-    
+ let theBody = "Hello"
+ let dissmis = true
+ let icon1 = 'https://www.freeiconspng.com/img/2743.png'
+  let options = {
+    requireInteraction: dissmis,
+    body: theBody,
+    icon: icon1
+}
         if (!("Notification" in window)) {
           alert("This browser does not support desktop notification");
         }
         // this should push your new notifications
         else if (Notification.permission === "granted") {
-         new Notification(props.notifyid);
+         new Notification(props.notifyid, options)
           Notification.onclose = function() {
           };
         }
